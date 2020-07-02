d048ca08578e9b2fcd41d046b4bc967cd4a6c91a
bansheerubber <gary.cactus.email@gmail.com>
2020-07-01 22:47:15 -0700

    added prototype map view

--- App.js
+++ App.js
@@ -8,106 +8,69 @@
 
 import React from 'react';
 import {
+  Dimensions,
   SafeAreaView,
   StyleSheet,
   ScrollView,
-  View,
-  Text,
+  TextInput,
   StatusBar,
+  View,
 } from 'react-native';
 
+import MapView from 'react-native-maps';
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  padding
+} from './helpers/style';
 
 const App: () => React$Node = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+
+        <View style={styles.searchContainer}>
+          <TextInput placeholder={'Search'} style={styles.search} />
+        </View>
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
-  engine: {
+  map: {
+    ...StyleSheet.absoluteFillObject,
+    zIndex: 0,
+  },
+  searchContainer: {
     position: 'absolute',
-    right: 0,
+    top: 25,
+    left: (Dimensions.get('window').width / 2) - (Dimensions.get('window').width * 0.9 / 2),
+    width: '90%',
+    height: 50,
+    zIndex: 5,
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  search: {
+    ...padding(10, 15, 10, 15),
+    backgroundColor: '#FFF',
+    color: '#333',
+    fontSize: 20,
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOpacity: 0.58,
+    elevation: 5,
   },
 });
 
