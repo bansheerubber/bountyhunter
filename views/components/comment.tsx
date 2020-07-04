dd5fdf7b515e6a3767fed5ca741e9d1cb5a641a5
bansheerubber <gary.cactus.email@gmail.com>
2020-07-03 18:07:31 -0700

    add comments options (like, share, etc)

--- views/components/comment.tsx
+++ views/components/comment.tsx
@@ -13,10 +13,12 @@ import {
 } from "../../helpers/style";
 
 import Image from 'react-native-scalable-image';
-
-// interface OwnProps {
-//   frog: string
-// }
+import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
+import {
+  faEllipsisH,
+  faShareSquare,
+  faThumbsUp,
+} from "@fortawesome/free-solid-svg-icons";
 
 interface OwnState {
   profilePictureWidth: number;
@@ -44,13 +46,19 @@ export class Comment extends React.Component<any, OwnState> {
           <Image width={this.state.profilePictureWidth} style={styles.profilePicture} source={{ uri:"https://bansheerubber.com/egg.png" }} />
         </View>
         <View style={styles.content}>
-          <View>
+          <View style={styles.contentMeta}>
             <Text style={styles.username}>username</Text>
           </View>
           <View>
             <Text style={styles.body}>
               Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph. A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long. Ultimately, a paragraph is a sentence or group of sentences that support one main idea. In this handout, we will refer to this as the “controlling idea,” because it controls what happens in the rest of the paragraph.
             </Text>
+          </View>
+          <View style={styles.commentOptions}>
+            <Text>0</Text>
+            <FontAwesomeIcon icon={ faThumbsUp } />
+            <FontAwesomeIcon icon={ faShareSquare } />
+            <FontAwesomeIcon icon={ faEllipsisH } />
           </View>
         </View>
       </View>
@@ -73,6 +81,16 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 4,
+  },
+  contentMeta: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  commentOptions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 100,
+    marginTop: 5,
   },
   username: {
     marginBottom: 5,
